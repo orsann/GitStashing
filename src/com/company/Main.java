4a7eaa7f9e7b302743ef9ccf43755733f155a2e9
Orsan <orsann@gmai.com>
2022-05-15 08:29:08 -0700

    the green color Auddii

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        basicFeatures Audii =new basicFeatures();
+        Audii.Color="pink";
+        Audii.regNo="BMW";
+        Audii.headlights="red";
+        Audii.engineCapac="v12";
+        Audii.doors="7";
+    }
+}
---
+++ src/com/company/basicFeatures.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class basicFeatures {
+
+        String engineCapac;
+        String Color;
+        String doors;
+        String headlights;
+        String regNo;
+    }
+
